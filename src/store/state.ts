455858ca6371190a759fe224dfacb615ff146920
Saad Farhan <sunehrafarhan123@gmail.com>
2021-11-14 02:19:52 +0500

    ⌨ STATE: Defined states

---
+++ src/store/state.ts
@@ -0,0 +1,25 @@
+const INITIAL_STATE = [
+    {
+        id: 1,
+        title: 'First post',
+        description: 'This is the first post',
+        imageUrl: 'https://picsum.photos/id/1/200/300',
+        price: 10,
+    },
+    {
+        id: 2,
+        title: 'Second post',
+        description: 'This is the second post',
+        imageUrl: 'https://picsum.photos/id/2/200/300',
+        price: 20,
+    }, 
+    {
+        id: 3,
+        title: 'Third post',
+        description: 'This is the third post',
+        imageUrl: 'https://picsum.photos/id/3/200/300',
+        price: 30,
+    }
+]
+
+export { INITIAL_STATE }
