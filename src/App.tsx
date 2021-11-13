a19d36d90acfbfee4551451317a213259f01dc90
Saad Farhan <sunehrafarhan123@gmail.com>
2021-11-14 02:25:27 +0500

    ⌨ PARENT COMPONENT: Imported the component and displayed within the Box component from MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { Box, Container } from "@material-ui/core";
+import Basket from "./components/basket/basket";
+import Product from "./components/product/product";
 
 const App = () => {
   return (
+    <Container maxWidth="md">
+      <Box mt={5} mb={5}>
+        <Product />
+      </Box>
+      <Box mt={5} mb={5}>
+        <Basket />
+      </Box>
+    </Container>
   );
 }
 
